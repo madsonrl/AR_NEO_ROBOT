0b8f7b1c77eaf191bf000ab981fbc61aca87def0
Madson Lemos <madsonlemos.es@gmail.com>
2022-01-28 10:45:35 -0400

    NEO ROBOT AR

---
+++ Assets/Sound.cs
@@ -0,0 +1,116 @@
+﻿using WebSocketSharp;
+using UnityEngine;
+using System;
+
+
+public class Sound : MonoBehaviour
+{
+    WebSocket ws;
+
+    public AudioSource Music;
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+
+
+    public string message;
+    public int audioIndex;
+
+
+    AudioClip soundClip(int audio)
+    {
+        return audioClipArray[audio];
+    }
+
+    void Start()
+    {
+        Music = GetComponent<AudioSource>();
+
+        message = "0";
+
+        ws = new WebSocket("ws://localhost:9500");
+
+        ws.OnMessage += (sender, e) =>
+        {
+
+            message = e.Data;
+        };
+
+        ws.Connect();
+    }
+
+
+    void Update()
+    {
+        if (ws == null)
+        {
+            return;
+        }
+
+        if (message != "")
+        {
+            audioIndex = Int32.Parse(message);
+        }
+
+        switch (message)
+        {
+            case "0":
+                Music.Stop();
+                break;
+            case "1":
+                Music.clip = soundClip(audioIndex);
+                if (!Music.isPlaying)
+                {
+                    Music.Play();
+                }
+                break;
+            case "2":
+                Music.clip = soundClip(audioIndex);
+                if (!Music.isPlaying)
+                {
+                    Music.Play();
+
+                }
+
+
+                break;
+            case "3":
+                Music.clip = soundClip(audioIndex);
+                if (!Music.isPlaying)
+                {
+                    Music.Play();
+                }
+                break;
+            case "4":
+                Music.clip = soundClip(audioIndex);
+                if (!Music.isPlaying)
+                {
+                    Music.Play();
+                }
+                break;
+            case "5":
+                Music.clip = soundClip(audioIndex);
+                if (!Music.isPlaying)
+                {
+                    Music.Play();
+                }
+                break;
+            case "6":
+                Music.clip = soundClip(audioIndex);
+                if (!Music.isPlaying)
+                {
+                    Music.Play();
+                }
+                break;
+            case "7":
+                Music.Stop();
+                break;
+
+
+        }
+
+    }
+
+
+}
+
---
+++ Assets/WSocket.cs
@@ -0,0 +1,116 @@
+﻿
+using WebSocketSharp;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class WSocket : MonoBehaviour
+{
+    WebSocket ws;
+
+    public Animator controllerRobot;
+    public string message;
+    public int animationIndex;
+    public GameObject Robot;
+
+
+    void Start()
+    {
+
+
+        // controllerRobot = GetComponent<Animator>();
+        //Robot = GetComponent<GameObject>();
+
+
+
+        //Robot = GameObject.Find("Robot Kyle");
+
+
+        ws = new WebSocket("ws://localhost:9500");
+
+        ws.OnMessage += (sender, e) =>
+        {
+
+            message = e.Data;
+        };
+
+        ws.Connect();
+    }
+
+
+    void Update()
+    {
+        if (ws == null)
+        {
+            return;
+        }
+
+
+
+        if (message != "")
+        {
+            animationIndex = Int32.Parse(message);
+        }
+
+        switch (message)
+        {
+            case "0":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+            case "1":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+            case "2":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+            case "3":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+            case "4":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+            case "5":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+            case "6":
+                controllerRobot.SetInteger("condicao", animationIndex);
+                break;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            ws.Send("0");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            ws.Send("1");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            ws.Send("2");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            ws.Send("3");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            ws.Send("4");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            ws.Send("5");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            ws.Send("6");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            ws.Send("7");
+        }
+
+
+    }
+}
